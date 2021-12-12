3d5a06adfd399d63f82b24c95d70886f04c70f9e
Mateusz Magdon <mateuszm@gmail.com>
2021-12-12 11:45:45 +0100

    9.2 minor changes

--- 9/2.py
+++ 9/2.py
@@ -3,24 +3,20 @@ def is_local_min(map, x, y):
     max_y = len(map[0])-1
     val = map[x][y]
 
-    if x > 0:
-        if val >= map[x-1][y]:
+    if x > 0 and val >= map[x-1][y]:
             return False
 
-    if x < max_x:
-        if val >= map[x+1][y]:
+    if x < max_x and val >= map[x+1][y]:
             return False
 
-    if y > 0:
-        if val >= map[x][y-1]:
+    if y > 0 and val >= map[x][y-1]:
             return False
 
-    if y < max_y:
-        if val >= map[x][y+1]:
+    if y < max_y and val >= map[x][y+1]:
             return False
     return True
 
-def calc_basin_size(map, x, y, start=True):
+def calc_basin_size(map, x, y):
     max_x = len(map)-1
     max_y = len(map[0])-1
 
@@ -31,29 +27,28 @@ def calc_basin_size(map, x, y, start=True):
         el = to_discover.pop()
         x = el[0]
         y = el[1]
-        if not start and map[x][y] == 0:
+        if map[x][y] == -1:
             continue
-        start = False
 
-        map[x][y] = 0
+        map[x][y] = -1
         total += 1
 
-        if x > 0 and map[x-1][y] not in [0, 9]:
+        if x > 0 and map[x-1][y] not in [-1, 9]:
             to_discover.append((x-1, y))
 
-        if x < max_x and map[x+1][y] not in [0, 9]:
+        if x < max_x and map[x+1][y] not in [-1, 9]:
             to_discover.append((x+1, y))
 
-        if y > 0 and map[x][y-1] not in [0, 9]:
+        if y > 0 and map[x][y-1] not in [-1, 9]:
             to_discover.append((x, y-1))
 
-        if y < max_y and map[x][y+1] not in [0, 9]:
+        if y < max_y and map[x][y+1] not in [-1, 9]:
             to_discover.append((x, y+1))
     
     return total
 
 
-lines = open("9/input.txt").readlines()
+lines = open("9/input_test.txt").readlines()
 map = [[int(el) for el in line.strip()] for line in lines]
 
 width = len(map[0])
